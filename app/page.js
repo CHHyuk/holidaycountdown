bc9c8e66dd1faa39ccfa235fb305423d08257303
CHHyuk <gksgur1038@naver.com>
2024-01-01 12:38:29 +0900

    기본 페이지 구조 파악

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+import Topbar from './components/Topbar.client'
+import Loading from './components/Loading.client'
+
+
+export default function Page() {
+  return (
+    <>
+    </>
+  )
+}
